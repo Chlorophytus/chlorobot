f9bd6f9902a3b3466195d1ae2f33c5a41526300c
Roland Metivier <metivier.roland@chlorophyt.us>
2024-11-07 17:31:49 -0600

    debug tracer

--- chloresolve/main.py
+++ chloresolve/main.py
@@ -369,7 +369,7 @@ async def main() -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format="[%(asctime)s] [%(name)s - %(levelname)s] %(message)s",
     )
     asyncio.run(main())
