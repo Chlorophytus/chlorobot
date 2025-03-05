4d6d754ec8f274262c3463ad3e328e74f134566f
Roland Metivier <metivier.roland@chlorophyt.us>
2025-03-05 16:07:18 -0600

    tweak init script

--- priv/init.lua
+++ priv/init.lua
@@ -1,4 +1,19 @@
 local joining = true
+local trigger = os.getenv("CHLOROBOT_TRIGGER")
+
+function chlorobot.parse_hostmask(hostmask)
+  local _, _, nickname, ident, cloak = string.find(hostmask,"(.+)!(.+)@(.+)")
+
+  return {
+    nickname = nickname,
+    ident = ident,
+    cloak = cloak,
+  }
+end
+
+function chlorobot.get_action(text)
+  return string.match(text, "\x01ACTION (.+)\x01")
+end
 
 function chlorobot.tick(packet)
   if packet ~= nil then
@@ -10,6 +25,21 @@ function chlorobot.tick(packet)
         },
       })
       joining = false
+    end
+
+    if packet.command == "PRIVMSG" then
+      local date = os.date("%x %X")
+      local host = chlorobot.parse_hostmask(packet.prefix)
+      local destination = packet.params[1]
+      local message = packet.trailing_param
+
+      local action = chlorobot.get_action(message)
+
+      if action ~= nil then
+        print(string.format("(%s) [%s] *** %s %s", date, destination, host.nickname, action))
+      else
+        print(string.format("(%s) [%s] <%s> %s", date, destination, host.nickname, message))
+      end
     end
   end
 end
