3e4beba8b04a6017fbc4461a56041309c4b0929a
Roland Metivier <metivier.roland@chlorophyt.us>
2026-08-15 18:28:59 -0500

    socket needs graceful disconnect in end

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,8 @@ int main(int argc, char **argv) {
             if (*command == "ERROR") {
               std::cerr << "Disconnected: "
                         << packet.trailing_param.value_or("???") << std::endl;
+
+              sock.disconnect();
               break;
             }
             if (*command == "PING") {
